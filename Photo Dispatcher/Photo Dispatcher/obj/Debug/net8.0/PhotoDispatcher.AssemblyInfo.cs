da3a93a11b347bd2a4d7b9bbc6873bf6104010f9
Acanfora Giuseppe <acanfora_7200@hotmail.com>
2024-06-18 12:06:21 +0200

    Project fix + adding photos for tests

--- Photo Dispatcher/Photo Dispatcher/obj/Debug/net8.0/PhotoDispatcher.AssemblyInfo.cs
+++ Photo Dispatcher/Photo Dispatcher/obj/Debug/net8.0/PhotoDispatcher.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhotoDispatcher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+798fdedda9fa7155afc9d89bd99ef6d4ffbf40c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bbfac32cf0cb4ef9857ed507238c6843d17f781")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhotoDispatcher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhotoDispatcher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
